4d419444dd579a13833b707c0407151dcbc6e56d
Mike Michaud <michaud.michael@gmail.com>
2018-08-01 22:11:18 -0700

    removed add_todo

--- client/src/reducers/reducer.js
+++ client/src/reducers/reducer.js
@@ -7,8 +7,9 @@ export default function (state = initialState, action) {
 		case 'GET_TODOS':
 			return {...state, todos: action.payload}
 
-		case 'ADD_TODO':
-			return {...state, todos: [...state.todos, action.payload]}
+		// case 'ADD_TODO':
+		// 	return {...state, todos: [...state.todos, action.payload]}
+		//not necessary since this adds directly to the list of todos. calling getStuff right after add will refresh the list
 
 		default:
 			return state
